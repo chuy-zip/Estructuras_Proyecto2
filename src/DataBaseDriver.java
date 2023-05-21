4305f9bfdc1b339e363452904360c7ecd0f33f85
wwIrvingww <mor22781@guvg.edu.gt>
2023-05-21 12:48:42 -0600

    se agregaron propiedades al NodoJuego

--- src/DataBaseDriver.java
+++ src/DataBaseDriver.java
@@ -63,10 +63,10 @@ public class DataBaseDriver implements AutoCloseable {
      * @param descripcion
      */
 
-    public void crearNodoJuego(String nombreJuego, String descripcion) {
+    public void crearNodoJuego(String nombreJuego, String descripcion, boolean nintendo, boolean pc, boolean mobile, boolean xbox, boolean playstation) {
         try (Session session = driver.session()) {
             session.writeTransaction(tx -> {
-                tx.run("CREATE (:Juego {nombre: $nombre, descripcion: $descripcion})", Values.parameters("nombre", nombreJuego, "descripcion", descripcion));
+                tx.run("CREATE (:Juego {nombre: $nombre, descripcion: $descripcion,  nintendo: $nintendo, pc: $pc, mobile: $mobile, xbox: $xbox, playstation: $playstation})", Values.parameters("nombre", nombreJuego, "descripcion", descripcion, "nintendo", nintendo, "pc", pc, "mobile", mobile, "xbox", xbox, "playstation", playstation));
                 return null;
             });
         }
